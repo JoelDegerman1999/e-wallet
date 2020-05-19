a81afed3f5a4c3c92bc51758d2334d5a630664a6
Joel Degerman <joel@joeldegerman.se>
2020-05-19 11:43:49 +0200

    Added form validation

--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -1,61 +1,107 @@
 <template>
   <section class="form">
-    <div class="cardnumber flex">
-      <label>CARD NUMBER</label>
-      <input type="text" v-model="newCard.cardNumber" />
-    </div>
-    <div class="cardholder flex">
-      <label>CARDHOLDER NAME</label>
-      <input type="text" v-model="newCard.name" />
-    </div>
-    <div class="valid-and-ccv">
-      <div class="valid">
-        <label>VALID THRU</label>
-        <input type="text" v-model="newCard.valid" />
+    <form @submit="checkForm">
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+       <ul>
+        <li v-for="(error, index) in errors" :key="index">{{error}}</li>
+      </ul>
+      </p>
+      <div class="cardnumber flex">
+        <label>CARD NUMBER</label>
+        <input type="text" v-model="formCard.cardNumber" @change="updateCardNumber" />
       </div>
-      <div class="ccv">
-        <label>CCV</label>
-        <input type="text" />
+      <div class="cardholder flex">
+        <label>CARDHOLDER NAME</label>
+        <input type="text" v-model="formCard.name" @change="updateCardName" />
       </div>
-    </div>
-    <div class="vendor flex">
-      <label>VENDOR</label>
-      <select name="vendors" id="vendors" v-model="vendor" @change="changeVendor">
-        <option value="bitcoin">Bitcoin</option>
-        <option value="blockchain">Blockchain</option>
-        <option value="evil">Evil Corp</option>
-        <option value="ninja">Ninja Corp</option>
-      </select>
-    </div>
-    <button @click="addCard">Create Card</button>
+      <div class="valid-and-ccv">
+        <div class="valid">
+          <label>VALID THRU</label>
+          <input type="text" v-model="formCard.valid" @change="updateCardValid" />
+        </div>
+        <div class="ccv">
+          <label>CCV</label>
+          <input type="text" />
+        </div>
+      </div>
+      <div class="vendor flex">
+        <label>VENDOR</label>
+        <select name="vendors" id="vendors" v-model="vendor" @change="updateVendor">
+          <option value="bitcoin">Bitcoin</option>
+          <option value="blockchain">Blockchain</option>
+          <option value="evil">Evil Corp</option>
+          <option value="ninja">Ninja Corp</option>
+        </select>
+      </div>
+      <button type="submit">Add Card</button>
+    </form>
   </section>
 </template>
 
 <script>
 export default {
   props: {
-    card: Object,
+    card: Object
   },
   data() {
     return {
       newCard: this.card,
+      formCard: {
+        cardNumber: "",
+        name: "",
+        valid: ""
+      },
       vendor: "bitcoin",
+      errors: []
     };
   },
   methods: {
-    changeVendor() {
-      this.newCard.vendor = this.vendor
+    updateVendor() {
+      this.newCard.vendor = this.vendor;
+    },
+    updateCardNumber() {
+      this.newCard.cardNumber = this.formCard.cardNumber;
+    },
+    updateCardName() {
+      this.newCard.name = this.formCard.name;
+    },
+    updateCardValid() {
+      this.newCard.valid = this.formCard.valid;
     },
     addCard() {
       if (this.$root.cardArray.length == 8) {
         alert("Maximum cards allowed, please remove one");
         return;
       }
-      this.newCard.vendor = this.vendor
       this.$emit("addCard", this.newCard);
     },
+    checkForm(e) {
+      if (
+        this.formCard.cardNumber &&
+        this.formCard.name &&
+        this.formCard.valid
+      ) {
+        this.addCard()
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.formCard.cardNumber) {
+        this.errors.push("Card number required");
+      }
+      if (!this.formCard.name) {
+        this.errors.push("Name required");
+      }
+      if (!this.formCard.valid) {
+        this.errors.push("Valid date required");
+      }
+
+      e.preventDefault();
+    }
   },
-  computed: {},
+  computed: {}
 };
 </script>
 
